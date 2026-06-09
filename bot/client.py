d14447adc88813aa7a7cca15f0622cf582265a3a
devesh0800 <deveshduhan@gmail.com>
2026-06-09 15:14:40 +0530

    Remove logs and cache files

--- bot/client.py
+++ bot/client.py
@@ -8,14 +8,12 @@ API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 
 def get_client():
-    client = Client(API_KEY, API_SECRET)
+    client = Client(
+        api_key=API_KEY,
+        api_secret=API_SECRET,
+        testnet=True
+    )
 
     client.FUTURES_URL = "https://testnet.binancefuture.com/fapi"
 
     return client
-
-print("\nSUCCESS")
-print("Order ID:", response.get("orderId"))
-print("Status:", response.get("status"))
-print("Executed Qty:", response.get("executedQty"))
-print("Avg Price:", response.get("avgPrice", "N/A"))
